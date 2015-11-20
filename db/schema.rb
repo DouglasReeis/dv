ddf590a8bdbfad05f1d6e6bc9f0571b8275283e3
Douglas Reis <douglasreis_93@hotmail.com>
2015-11-19 22:26:28 -0200

    post composer

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120000933) do
+ActiveRecord::Schema.define(version: 20151120002501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gyms", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/gyms.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :gym do
+    name "MyString"
+address "MyString"
+phone "MyString"
+email "MyString"
+  end
+
+end
